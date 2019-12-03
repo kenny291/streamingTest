c0e16be5dbfdcf949bbb62c85bcb073b3fbe151b
Khiem Ta <khiemtd@fimplus.vn>
2019-12-03 11:27:50 +0700

    add clickhouse test

---
+++ src/main/java/com/kafkaDemo/AppClickhouse.java
@@ -0,0 +1,24 @@
+package com.kafkaDemo;
+
+import java.util.Arrays;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class App1 {
+   public static void main(String[] args) throws Exception {
+      import ru.yandex.clickhouse.ClickHouseStatement;
+ClickHouseStatement sth = connection.createStatement();
+sth.write().send("INSERT INTO test.writer", new ClickHouseStreamCallback() {
+    @Override
+    public void writeTo(ClickHouseRowBinaryStream stream) throws IOException {
+        for (int i = 0; i < 10; i++) {
+            stream.writeInt32(i);
+            stream.writeString("Name " + i);
+        }
+    }
+},
+ClickHouseFormat.RowBinary);
+      }
+   }
+}
